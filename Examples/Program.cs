36c6cf4ca9953c434b579c86a070fd685bb8631e
Melvyn <melvyn.laily@gmail.com>
2013-07-30 12:52:21 -0400

    added some simple but (somewhat) useful examples

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -4,20 +4,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace Examples
 {
 	class Program
 	{
 
-		/// <summary>
-		/// Not a very useful example, just need a main class to compile and run the tests.  
-		/// </summary>
 		public static void Main(string[] args)
 		{
 			Promise<string> hello = CreateSimplePromise();
-			hello.Success(mesg => Console.WriteLine(mesg));
+			hello.Success(mesg => Log(mesg));
+
+			var webPromise = GetWebPagePromise("http://google.com");
+			webPromise.Fail(error => Log(error.ToString()));
+			webPromise.Success(x => Log("Web request successfully executed!"));
+
+			var lastLinePromise = webPromise.FlatMap(webResponse => GetLastLine(webResponse.GetResponseStream()));
+			lastLinePromise.Fail(error => Log(error.ToString()));
+			lastLinePromise.Success(lastLine => Log("last line of the requested web page:\n{0}", lastLine));
+
 			Console.ReadLine();
+		}
+
+		private static void Log(string format, params object[] args)
+		{
+			Console.WriteLine("{0:HH:mm:ss:fff}: {1}", DateTime.Now, string.Format(format, args));
 		}
 
 		/// <summary>
@@ -33,6 +46,50 @@ namespace Examples
 				cb(null, "Hello World");
 			};
 
+			return new Promise<string>(constructor);
+		}
+
+		public static Promise<HttpWebResponse> GetWebPagePromise(string address)
+		{
+			Action<Action<PromiseError, HttpWebResponse>> constructor = (cb) =>
+			{
+				try
+				{
+					var request = HttpWebRequest.CreateHttp(address);
+					cb(null, (HttpWebResponse)request.GetResponse());
+				}
+				catch (Exception ex)
+				{
+					cb(new PromiseError(ex), null);
+				}
+			};
+			return new Promise<HttpWebResponse>(constructor);
+		}
+
+		public static Promise<string> GetLastLine(Stream stream)
+		{
+			Action<Action<PromiseError, string>> constructor = (cb) =>
+			{
+				try
+				{
+					string lastLine = null;
+					using (stream)
+					using (var sr = new StreamReader(stream))
+					{
+						while (!sr.EndOfStream)
+						{
+							lastLine = sr.ReadLine();
+							//artificial delay
+							System.Threading.Thread.Sleep(50);
+						}
+					}
+					cb(null, lastLine);
+				}
+				catch (Exception ex)
+				{
+					cb(new PromiseError(ex), null);
+				}
+			};
 			return new Promise<string>(constructor);
 		}
 
